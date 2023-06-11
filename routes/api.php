b4575df7aa015701c50c2e22d0535cf10f9cfd4a
FernandoMacia <fernando.macia@hotmail.es>
2023-06-11 13:54:06 +0200

    Cambio login a post

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/user', [AuthController::class, 'user']);
